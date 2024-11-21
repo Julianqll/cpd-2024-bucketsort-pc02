43037c73e8d3d1cd3765bbf4dc347450954dc433
rommelqcruz <rommel.quintanillac@usil.pe>
2024-11-21 08:27:20 +0100

    Formatting

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
+#include "bucketsort.h"
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include "bucketsort.h"
 
 #define LENGTH 8
 
@@ -11,54 +11,54 @@ char *strings;
 long int N;
 
 void openfiles() {
-	fin = fopen("bucketsort.in", "r+");
-	if (fin == NULL) {
-		perror("fopen fin");
-		exit(EXIT_FAILURE);
-	}
+  fin = fopen("bucketsort.in", "r+");
+  if (fin == NULL) {
+    perror("fopen fin");
+    exit(EXIT_FAILURE);
+  }
 
-	fout = fopen("bucketsort.out", "w");
-	if (fout == NULL) {
-		perror("fopen fout");
-		exit(EXIT_FAILURE);
-	}
+  fout = fopen("bucketsort.out", "w");
+  if (fout == NULL) {
+    perror("fopen fout");
+    exit(EXIT_FAILURE);
+  }
 }
 
 void closefiles(void) {
-	fclose(fin);
-	fclose(fout);
+  fclose(fin);
+  fclose(fout);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-	long int i, *r;
+  long int i, *r;
 
-	openfiles();
+  openfiles();
 
-	fscanf(fin, "%ld", &N);
-	strings = (char*) malloc(N * LENGTH);
-	if (strings == NULL) {
-		perror("malloc strings");
-		exit(EXIT_FAILURE);
-	}
+  fscanf(fin, "%ld", &N);
+  strings = (char *)malloc(N * LENGTH);
+  if (strings == NULL) {
+    perror("malloc strings");
+    exit(EXIT_FAILURE);
+  }
 
-	for (i = 0; i < N; i++){
-		fscanf(fin, "%s", strings + (i * LENGTH));
+  for (i = 0; i < N; i++) {
+    fscanf(fin, "%s", strings + (i * LENGTH));
     printf("%s", strings + (i * LENGTH));
   }
 
-	fflush(stdout);
+  fflush(stdout);
 
-	r = bucket_sort(strings, LENGTH, N);
+  r = bucket_sort(strings, LENGTH, N);
 
-	fflush(stdout);
+  fflush(stdout);
 
-	for (i = 0; i < N; i++)
-		fprintf(fout, "%s\n", strings + (r[i] * LENGTH));
+  for (i = 0; i < N; i++)
+    fprintf(fout, "%s\n", strings + (r[i] * LENGTH));
 
-	free(r);
-	free(strings);
-	closefiles();
+  free(r);
+  free(strings);
+  closefiles();
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
